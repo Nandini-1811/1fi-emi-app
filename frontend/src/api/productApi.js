fef1ef8c3014aeeede44c8cd140bc58901f7166b
Nandini Goel <nandinigoel1805@gmail.com>
2026-09-03 15:15:06 +0530

    Use environment variable for API base URL

--- frontend/src/api/productApi.js
+++ frontend/src/api/productApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api/products';
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 export const fetchAllProducts = async () => {
     const response = await axios.get(API_BASE_URL);
@@ -11,5 +11,3 @@ export const fetchProductBySlug = async(slug) => {
     const response = await axios.get(`${API_BASE_URL}/${slug}`);
     return response.data;
 };
-
-
